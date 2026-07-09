b73096792819218083f392ff76a29d76b33c9671
royYang <inguz@naver.com>
2026-07-09 15:04:50 +0900

    Add Launchicken screenshot scaffold: launchicken/scaffold/android/LaunchickenScreenshotTest.kt

---
+++ launchicken/scaffold/android/LaunchickenScreenshotTest.kt
@@ -0,0 +1,9 @@
+package dev.launchicken.scaffold
+
+/*
+ * Move this file into app/src/androidTest/... after adding your AndroidX test dependencies.
+ * Route your app with intent extra "launchicken_screen" and capture screenshots from the test.
+ */
+class LaunchickenScreenshotTestScaffold {
+    val launchickenScreenExtra = "launchicken_screen"
+}
